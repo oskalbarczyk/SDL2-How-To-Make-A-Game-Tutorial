0ab29b9187e4c6019bf96b175357576ca673a89c
oskalbarczyk <oskalbarczyk@gmail.com>
2023-08-31 17:12:10 +0200

    #3 Limiting Frame Rate

--- main.cpp
+++ main.cpp
@@ -8,14 +8,30 @@ Game *game = nullptr;
 
 int main() {
 
-    game = new Game();
+    const int FPS = 60;
+    const int frameDelay = 1000/FPS;
 
-    game->init("BirchEngine",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600, false);
+    Uint32 frameStart;
+    int frameTime;
+
+
+    game = new Game();
+    game->init("GameWindow",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600, false);
 
     while(game->running()){
+
+        frameStart = SDL_GetTicks();
+
         game->handleEvents();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameDelay > frameTime){
+            SDL_Delay(frameDelay - frameTime);
+        }
+
     }
 
     game->clean();
