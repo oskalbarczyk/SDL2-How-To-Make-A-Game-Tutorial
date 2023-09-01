246e69a5d61baf2882e684590d6068a8ff211786
oskalbarczyk <oskalbarczyk@gmail.com>
2023-09-01 22:32:00 +0200

    #19 Scrolling Tile Maps

--- ECS/TransformComponent.h
+++ ECS/TransformComponent.h
@@ -6,48 +6,47 @@ class TransformComponent : public Component
 {
 
 public:
-	
-	Vector2D position;
-	Vector2D velocity;
 
-	int height = 32;
-	int width = 32;
-	int scale = 1;
+    Vector2D position;
+    Vector2D velocity;
 
-	int speed = 3;
+    int height = 32;
+    int width = 32;
+    int scale = 1;
 
-	TransformComponent()
-	{
-		position.Zero();
-	}
+    int speed = 3;
 
-	TransformComponent(int sc)
-	{
-		position.Zero();
-		scale = sc;
-	}
+    TransformComponent()
+    {
+        position.Zero();
+    }
 
-	TransformComponent(float x, float y)
-	{
-		position.Zero();
-	}
+    TransformComponent(int sc)
+    {
+        position.Zero();
+        scale = sc;
+    }
 
-	TransformComponent(float x, float y, int h, int w, int sc)
-	{
-		position.x = x;
-		position.y = y;
-		height = h;
-		width = w;
-		scale = sc;
-	}
+    TransformComponent(float x, float y)
+    {
+        position.Zero();
+    }
 
-	void init() override
-	{
-		velocity.Zero();
-	}
-	void update() override
-	{
-		position.x += velocity.x * speed;
-		position.y += velocity.y * speed;
-	}
+    TransformComponent(float x, float y, int h, int w, int sc)
+    {
+        position.x = x;
+        position.y = y;
+        height = h;
+        width = w;
+        scale = sc;
+    }
+
+    void init() override
+    {
+        velocity.Zero();
+    }
+
+    void update() override
+    {
+    }
 };
--- Game.cpp
+++ Game.cpp
@@ -12,6 +12,7 @@ SDL_Renderer* Game::renderer = nullptr;
 SDL_Event Game::event;
 
 std::vector<ColliderComponent*> Game::colliders;
+bool Game::isRunning = false;
 
 auto& player(manager.addEntity());
 auto& wall(manager.addEntity());
@@ -20,12 +21,15 @@ const char* mapfile = "assets/terrain_ss.png";
 
 enum groupLabels : std::size_t
 {
-	groupMap,
-	groupPlayers,
-	groupEnemies,
-	groupColliders
+    groupMap,
+    groupPlayers,
+    groupEnemies,
+    groupColliders
 };
 
+auto& tiles(manager.getGroup(groupMap));
+auto& players(manager.getGroup(groupPlayers));
+auto& enemies(manager.getGroup(groupEnemies));
 
 Game::Game()
 {}
@@ -35,96 +39,96 @@ Game::~Game()
 
 void Game::init(const char* title, int width, int height, bool fullscreen)
 {
-	int flags = 0;
-	
-	if (fullscreen)
-	{
-		flags = SDL_WINDOW_FULLSCREEN;
-	}
+    int flags = 0;
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-	{
-		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
-		renderer = SDL_CreateRenderer(window, -1, 0);
-		if (renderer)
-		{
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		}
+    if (fullscreen)
+    {
+        flags = SDL_WINDOW_FULLSCREEN;
+    }
 
-		isRunning = true;
-	}
-	map = new Map();
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
+        window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        if (renderer)
+        {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        }
 
-	//ecs implementation
+        isRunning = true;
+    }
+    map = new Map();
 
-	Map::LoadMap("assets/map.map", 25, 20);
+    //ecs implementation
 
-	player.addComponent<TransformComponent>(4);
-	player.addComponent<SpriteComponent>("assets/player_anims.png", true);
-	player.addComponent<KeyboardController>();
-	player.addComponent<ColliderComponent>("player");
-	player.addGroup(groupPlayers);
+    Map::LoadMap("assets/map.map", 25, 20);
+
+    player.addComponent<TransformComponent>(400, 320, 32 , 32, 4);
+    player.addComponent<SpriteComponent>("assets/player_anims.png", true);
+    player.addComponent<KeyboardController>();
+    player.addComponent<ColliderComponent>("player");
+    player.addGroup(groupPlayers);
 }
 
 void Game::handleEvents()
 {
-	
-	SDL_PollEvent(&event);
 
-	switch (event.type)
-	{
-	case SDL_QUIT :
-		isRunning = false;
-		break;
-	default:
-		break;
-	}
+    SDL_PollEvent(&event);
+
+    switch (event.type)
+    {
+        case SDL_QUIT :
+            isRunning = false;
+            break;
+        default:
+            break;
+    }
 }
 
 
 void Game::update()
 {
-	manager.refresh();
-	manager.update();
+    manager.refresh();
+    manager.update();
 
-	for (auto cc : colliders)
-	{
-		Collision::AABB(player.getComponent<ColliderComponent>(), *cc);
-	}
+    Vector2D pVel = player.getComponent<TransformComponent>().velocity;
+    int pSpeed = player.getComponent<TransformComponent>().speed;
+
+    for (auto t : tiles)
+    {
+        t->getComponent<TileComponent>().destRect.x += -(pVel.x * pSpeed);
+        t->getComponent<TileComponent>().destRect.y += -(pVel.y * pSpeed);
+    }
 }
-
-auto& tiles(manager.getGroup(groupMap));
-auto& players(manager.getGroup(groupPlayers));
-auto& enemies(manager.getGroup(groupEnemies));
 
 void Game::render()
 {
-	SDL_RenderClear(renderer);
-	for (auto& t : tiles)
-	{
-		t->draw();
-	}
-	for (auto& p : players)
-	{
-		p->draw();
-	}
-	for (auto& e : enemies)
-	{
-		e->draw();
-	}
-	SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+    for (auto& t : tiles)
+    {
+        t->draw();
+    }
+    for (auto& p : players)
+    {
+        p->draw();
+    }
+    for (auto& e : enemies)
+    {
+        e->draw();
+    }
+    SDL_RenderPresent(renderer);
 }
 
 void Game::clean()
 {
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-	SDL_Quit();
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
 }
 
 void Game::AddTile(int srcX, int srcY, int xpos, int ypos)
 {
-	auto& tile(manager.addEntity());
-	tile.addComponent<TileComponent>(srcX, srcY, xpos, ypos, mapfile);
-	tile.addGroup(groupMap);
+    auto& tile(manager.addEntity());
+    tile.addComponent<TileComponent>(srcX, srcY, xpos, ypos, mapfile);
+    tile.addGroup(groupMap);
 }
