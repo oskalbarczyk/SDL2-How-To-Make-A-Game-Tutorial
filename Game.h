25e1040213accc63e1b4810b11d591cc081f29c2
oskalbarczyk <oskalbarczyk@gmail.com>
2023-09-01 16:41:43 +0200

    #13 Tile Component & More Collision Detection

--- Game.h
+++ Game.h
@@ -4,6 +4,9 @@
 #include "SDL2/SDL.h"
 #include "SDL_image.h"
 #include <iostream>
+#include <vector>
+
+class ColliderComponent;
 
 class Game {
 public:
@@ -25,6 +28,7 @@ public:
 
     static SDL_Renderer* renderer;
     static SDL_Event event;
+    static std::vector<ColliderComponent*> colliders;
 
 
 private:
