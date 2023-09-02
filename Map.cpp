33b7d62f62905d77b557dea32d88b5825089472f
oskalbarczyk <oskalbarczyk@gmail.com>
2023-09-02 12:08:11 +0200

    #21 Tile Map Collision Parsing

--- Map.cpp
+++ Map.cpp
@@ -1,9 +1,14 @@
 #include "Map.h"
 #include "Game.h"
 #include <fstream>
+#include "ECS/ECS.h"
+#include "ECS/Components.h"
 
-Map::Map()
+extern Manager manager;
+
+Map::Map(const char* mfp, int ms, int ts) : mapFilePath(mfp), mapScale(ms), tileSize(ts)
 {
+    scaledSize = ms * ts;
 }
 
 Map::~Map()
@@ -12,24 +17,49 @@ Map::~Map()
 
 void Map::LoadMap(std::string path, int sizeX, int sizeY)
 {
-	char c;
-	std::fstream mapFile;
-	mapFile.open(path);
+    char c;
+    std::fstream mapFile;
+    mapFile.open(path);
 
-	int srcX, srcY;
+    int srcX, srcY;
 
-	for (int y = 0; y < sizeY; y++)
-	{
-		for (int x = 0; x < sizeX; x++)
-		{
-			mapFile.get(c);
-			srcY = atoi(&c) * 32;
-			mapFile.get(c);
-			srcX = atoi(&c) * 32;
-			Game::AddTile(srcX, srcY, x * 64, y * 64);
-			mapFile.ignore();
-		}
-	}
+    for (int y = 0; y < sizeY; y++)
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            mapFile.get(c);
+            srcY = atoi(&c) * tileSize;
+            mapFile.get(c);
+            srcX = atoi(&c) * tileSize;
+            AddTile(srcX, srcY, x * scaledSize, y * scaledSize);
+            mapFile.ignore();
+        }
+    }
 
-	mapFile.close();
+    mapFile.ignore();
+
+    for (int y = 0; y < sizeY; y++)
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            mapFile.get(c);
+            std::cout << c << std::endl;
+            if (c == '1')
+            {
+                auto& tcol(manager.addEntity());
+                tcol.addComponent<ColliderComponent>("terrain", x * scaledSize, y * scaledSize, scaledSize);
+                tcol.addGroup(Game::groupColliders);
+            }
+            mapFile.ignore();
+        }
+    }
+
+    mapFile.close();
+}
+
+void Map::AddTile(int srcX, int srcY, int xpos, int ypos)
+{
+    auto& tile(manager.addEntity());
+    tile.addComponent<TileComponent>(srcX, srcY, xpos, ypos, tileSize, mapScale, mapFilePath);
+    tile.addGroup(Game::groupMap);
 }
--- Map.h
+++ Map.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <string>
 
-class Map 
+class Map
 {
 public:
-	 
-	Map();
-	~Map();
 
-	static void LoadMap(std::string path, int sizeX, int sizeY);
+    Map(const char* mfp, int ms, int ts);
+    ~Map();
+
+    void LoadMap(std::string path, int sizeX, int sizeY);
+    void AddTile(int srcX, int srcY, int xpos, int ypos);
 
 private:
-
+    const char* mapFilePath;
+    int mapScale;
+    int tileSize;
+    int scaledSize;
 
 };
